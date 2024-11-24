5438dcfe11a35132e0c7778766df3ade461808f1
McSohan <mcsohan6300@gmail.com>
2024-11-24 13:13:38 -0800

    fix electron forge build issue

--- application/forge.config.js
+++ application/forge.config.js
@@ -9,7 +9,10 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        authors: 'Sohan',
+        description: 'Small application to setup github CICD',
+      },
     },
     {
       name: '@electron-forge/maker-zip',
@@ -17,11 +20,17 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        authors: 'Sohan',
+        description: 'Small application to setup github CICD',
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        authors: 'Sohan',
+        description: 'Small application to setup github CICD',
+      },
     },
   ],
   plugins: [
